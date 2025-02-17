d6d214ed41f48c63f1b7232122554de0b7ba2b81
baisali609 <baisali609@gmail.com>
2025-02-17 21:53:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+document.addEventListener("DOMContentLoaded", ()=>{
+	const images = document.querySelectorAll(".image");
+	let dragElement;
+
+	images.forEach((image), =>{
+		image.addEventListener("dragstart", (e)=>{
+			dragElement = e.target;
+			e.dataTransfer.setData("text/plain", "");
+		});
+		image.addEventListener("dragover",(e)=>{
+			e.preventDefault();
+		});
+		image.addEventListener("drop", (e)=>{
+			e.preventDefault();
+			if(dragElement !== e.target){
+				let temp =dragElement.innerHTML;
+				dragElement.innerHTML = e.target.innerHTML;
+				e.target.innerHTML = temp;
+			}
+		});
+	});
+});
